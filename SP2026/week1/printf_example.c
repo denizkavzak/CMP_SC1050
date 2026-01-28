c214be4ba08b5ae929b1273b7452d559e59befa5
Deniz Kavzak <deniz.kavzak@gmail.com>
2026-01-28 14:27:54 -0600

    A simple printf example printing text is pushed

---
+++ SP2026/week1/printf_example.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+
+    printf("Welcome ");
+    printf("to C!\n");
+
+    printf("Welcome to C!\n");
+
+    printf("Welcome \n to \n C!\n");
+
+    printf("Welcome\n");
+    printf("to\n");
+    printf("C!\n");
+
+    return 0;
+}
