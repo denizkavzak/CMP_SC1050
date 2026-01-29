b08ef82e01946fd878825857fe93a2ddfa94567d
Deniz Kavzak <deniz.kavzak@gmail.com>
2026-01-29 17:12:10 -0600

    Remainder example added to integer division example

--- SP2026/week2/integer_div.c
+++ SP2026/week2/integer_div.c
@@ -2,13 +2,40 @@
 
 int main(){
 
-    int a = 7;
-    int b = 2;
+    int num1 = 7;
+    int num2 = 2;
 
-    int div = a/b;
-    printf("Div of a:%d and b:%d is: %d\n", a,b,div);
-    printf("Div of a:%d and b:%d is: %d\n", a,b,a/b);
+    int div = 7/2;
+    int rem = 7%2;
     
+    printf("num1: %d divided by num2: %d = %d\n", num1, num2, div);
+    printf("Remainder of num1: %d divided by num2: %d = %d\n", num1, num2, rem);
+
     return 0;
 
 }
+
+
+
+
+
+
+
+
+
+
+
+// #include<stdio.h>
+
+// int main(){
+
+//     // int a = 7;
+//     // int b = 2;
+
+//     // int div = a/b;
+//     // printf("Div of a:%d and b:%d is: %d\n", a,b,div);
+//     // printf("Div of a:%d and b:%d is: %d\n", a,b,a/b);
+    
+//     // return 0;
+
+// }
