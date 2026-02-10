930e1af6303b116b5d03ff1a1fd362710e11c1e3
Deniz Kavzak <deniz.kavzak@gmail.com>
2026-02-10 14:19:53 -0600

    Example comparing while, do while and for loop is added

---
+++ SP2026/week4/while_for_dowhile.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main(){
+
+    int counter = 1;
+    while(counter <= 10){
+        printf("%d\n", counter);
+        counter++;
+    }
+
+    counter = 0; // has to be 0
+    while(++counter <= 10){ // increment first, then check condition
+        printf("%d\n", counter);
+    }
+
+    for(counter = 1; counter <= 10: counter++){
+        printf("%d\n", counter);
+    }
+
+    counter = 11;
+    do{ // will execute at least ONCE before the check
+        printf("%d\n", counter);
+        counter++;
+    }while(counter <= 10);
+
+    return 0;
+}
