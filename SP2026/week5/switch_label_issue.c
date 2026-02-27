988469b0e5d23eb9c06ea1b3f73edf6a7e57ed7a
Deniz Kavzak <deniz.kavzak@gmail.com>
2026-02-27 12:07:31 -0600

    Example on condition in switch case mistake

---
+++ SP2026/week5/switch_label_issue.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+
+    int num = 2;
+
+    switch (num == 2) {
+
+        case 1:
+            printf("Correct\n");
+            break;
+
+        case 2:
+            printf("Wrong\n");
+            break;
+    }
+
+    return 0;
+}
