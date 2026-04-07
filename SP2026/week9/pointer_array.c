7a35548d4e3d2e4c4577fabd15e922e9e734e82e
Deniz Kavzak <deniz.kavzak@gmail.com>
2026-04-07 12:06:42 -0500

    Example on how pointer arithmetic is used w arrays
    Example on how pointer arithmetic is used
    with arrays. Accessing elements of the array
    using pointer arithmetic on the array itself.

---
+++ SP2026/week9/pointer_array.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main(){
+
+    int a[] = {1,2,3,4,5};
+    int *p = a;
+
+    printf("value of a is: %p\n", a);
+    printf("address of a is: %p\n", &a);
+    printf("value of p is: %p\n", p);
+    
+    printf("value of a[0] is: %d\n", a[0]);
+    printf("value of *p is: %d\n", *p);
+
+    printf("value of *(p+1) is: %d\n", *(p+1));
+    printf("value of *(p+2) is: %d\n", *(p+2));
+
+    printf("value of *(a+1) is: %d\n", *(a+1));
+    printf("value of *(a+2) is: %d\n", *(a+2));
+
+    return 0;
+}
