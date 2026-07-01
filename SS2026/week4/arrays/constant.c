2555a275a3187869d3e66415d5f2eca79cdbe712
Deniz Kavzak <deniz.kavzak@gmail.com>
2026-07-01 12:20:30 -0500

    Example of a symbolic constant SIZE to be used in arrays

---
+++ SS2026/week4/arrays/constant.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+#define SIZE 10
+
+int main(){
+
+    int arr[SIZE] = {0};
+
+    for (int i = 0; i < SIZE; i++)
+    {
+        printf("%d\n",arr[i]);
+    }
+
+    return 0;
+
+}
