7f4b632572951820855354012463997af50caf2c
Deniz Kavzak <deniz.kavzak@gmail.com>
2026-07-10 09:24:07 -0500

    Example on linear search

---
+++ SS2026/week5/search/linear.c
@@ -0,0 +1,12 @@
+int linearSearch(const int numbers[], int size, int target)
+{
+    for (int i = 0; i < size; i++)
+    {
+        if (numbers[i] == target)
+        {
+            return i;
+        }
+    }
+
+    return -1;
+}
