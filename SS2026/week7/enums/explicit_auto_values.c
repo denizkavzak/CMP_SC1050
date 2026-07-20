82b1350b307943a45a3ae8023d40ff102bcda13f
Deniz Kavzak <deniz.kavzak@gmail.com>
2026-07-20 13:21:25 -0500

    Example of using explicit + auto incremented enum values
    When we explicitly give a value to an enum member, the next
    members will get the assigned value + 1, and continue to
    get incremented by 1s.

---
+++ SS2026/week7/enums/explicit_auto_values.c
@@ -0,0 +1,15 @@
+enum Result
+{
+    SUCCESS = 0,
+    FILE_ERROR = 10,
+    NETWORK_ERROR, // will get value 11
+    PERMISSION_ERROR // will get value 12
+};
+
+enum Code
+{
+    A = 5,
+    B, // will get value 6
+    C = 20,
+    D // will get value 21
+};
