b8688bb9e93196c482d2652b437482be94cf3703
Deniz Kavzak <deniz.kavzak@gmail.com>
2026-07-20 13:18:24 -0500

    Meaningful examples where using duplicate enum values

---
+++ SS2026/week7/enums/duplicate_use.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+enum AccessResult
+{
+    ACCESS_GRANTED = 0,
+    ACCESS_DENIED = 1,
+    ACCESS_FORBIDDEN = 1
+};
+
+enum Boolean
+{
+    FALSE = 0,
+    NO = 0,
+    TRUE = 1,
+    YES = 1
+};
