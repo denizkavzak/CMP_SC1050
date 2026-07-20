0ed9adf9d805bbddae5b1207746c4bbb67ee5a1f
Deniz Kavzak <deniz.kavzak@gmail.com>
2026-07-20 13:02:28 -0500

    Example of defining struct variables

---
+++ SS2026/week7/structs/definitions.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+struct student{
+    int id;
+    char name[20];
+    double gpa;
+}st1, st2, st3, studentArray[5];
+
+int main(){
+    struct student st4;
+    // struct student st1, st2, st3;
+    // struct student studentArray[5];
+
+    return 0;
+}
